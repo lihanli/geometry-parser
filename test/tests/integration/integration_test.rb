374321a63c8032861822a84e62adcf2874b6e2ae
Lihan Li <frankieteardrop@gmail.com>
2014-01-04 18:11:35 -0500

    dont need to run ruby on executable file

--- test/tests/integration/integration_test.rb
+++ test/tests/integration/integration_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class IntegrationTest < MiniTest::Unit::TestCase
   def test
-    assert_equal("odd is not a convex polygon\nsquare surrounds triangle\nsquare intersects rectangle\nsquare is separate from pentagon\ntriangle is inside square\ntriangle intersects rectangle\ntriangle is separate from pentagon\nrectangle intersects square\nrectangle intersects triangle\nrectangle is separate from pentagon\npentagon is separate from square\npentagon is separate from triangle\npentagon is separate from rectangle\n", `bundle exec ruby bin/geometry_parser test/tests/integration/data/data.json`)
+    assert_equal("odd is not a convex polygon\nsquare surrounds triangle\nsquare intersects rectangle\nsquare is separate from pentagon\ntriangle is inside square\ntriangle intersects rectangle\ntriangle is separate from pentagon\nrectangle intersects square\nrectangle intersects triangle\nrectangle is separate from pentagon\npentagon is separate from square\npentagon is separate from triangle\npentagon is separate from rectangle\n", `bundle exec bin/geometry_parser test/tests/integration/data/data.json`)
   end
 end
